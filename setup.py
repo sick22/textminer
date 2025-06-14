117994576dc826bf28b7f59e099254c19b21cd60
sick22 <jeongyongseok.dev@gmail.com>
2025-06-15 01:34:20 +0900

    0.1.15

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ long_description = (this_dir / "README.md").read_text(encoding="utf-8")
 
 setup(
     name="textminer-pro-jys",
-    version="0.1.14",                             
+    version="0.1.15",                             
     description="Advanced text mining utils",
     long_description=long_description,            
     long_description_content_type="text/markdown",
